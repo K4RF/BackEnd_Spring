2411c7923f7d3eef904052cfd5bac299def66e23
K4RF <rpdirwk1025@naver.com>
2024-01-03 20:10:07 +0900

    appconfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.dicount.DiscountPolicy;
 import hello.core.dicount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,10 +10,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(memberRepository());
     }   // 생성자 주입 방식
 
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
