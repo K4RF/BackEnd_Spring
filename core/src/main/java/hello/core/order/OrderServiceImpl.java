a7b2f5d3aca3b11edbf0356164a290e3694e84ae
KaarfSub <rpdirwk1025@naver.com>
2024-01-16 09:08:34 +0900

    Add Lombok

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,21 +6,25 @@ import hello.core.dicount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements  OrderService{
     private final MemberRepository memberRepository;
 
     private final DiscountPolicy discountPolicy;
     //private final RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
 
-    @Autowired
+   /*
+   @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
