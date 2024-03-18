20e56a3e8ffb0741bc88a23cebe1cce6bd699b3c
K4RF <rpdirwk1025@naver.com>
2024-03-18 21:54:20 +0900

    불필요한 코드 삭제

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -2,11 +2,9 @@ package hello.servlet.web.frontcontroller.v2;
 
 import hello.servlet.web.frontcontroller.MyView;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 
 public interface ControllerV2 {
