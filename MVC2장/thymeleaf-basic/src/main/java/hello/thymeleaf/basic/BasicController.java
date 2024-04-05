b9855a2ead6bb220150b9dc669e140dafd390154
KaarfSub <rpdirwk1025@naver.com>
2024-04-05 16:13:59 +0900

    text - unescaped, 타임리프 추가

--- MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,5 +1,4 @@
 package hello.thymeleaf.basic;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/basic")
-public class BasicController {
-    @GetMapping("/text-basic")
-    public String textBasic(Model model) {
+public class BasicController{
+
+    @GetMapping("text-basic")
+    public String textBasic(Model model){
         model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";
+    }
+
+    @GetMapping("text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
     }
 }
