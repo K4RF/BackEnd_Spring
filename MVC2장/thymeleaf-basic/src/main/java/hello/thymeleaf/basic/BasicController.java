62787b17b90a28b3458968a3ba68b324e7fc465d
K4RF <rpdirwk1025@naver.com>
2024-04-06 20:36:52 +0900

    속성 값 설정

--- MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -93,6 +93,10 @@ public class BasicController{
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
     @Data
     static  class User{
         private String username;
