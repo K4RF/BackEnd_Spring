5116d1ef0b9fc47d21a1300b3c58f3f5a7886e6d
K4RF <rpdirwk1025@naver.com>
2024-04-06 20:54:04 +0900

    타임리프 주석

--- MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ MVC2장/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -119,6 +119,13 @@ public class BasicController{
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
     @Data
     static  class User{
         private String username;
