f37f73bc2e75e30b8595678dfa1a92b1cd13dbab
KaarfSub <rpdirwk1025@naver.com>
2024-05-27 15:34:13 +0900

    런타임 예외 적용

---
+++ SpringDB/DB1/jdbc/src/main/java/hello/jdbc/Repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.jdbc.Repository;
+
+import hello.jdbc.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
