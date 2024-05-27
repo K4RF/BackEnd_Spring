a42937a283974a8cdd797766b7775622f30a4edd
K4RF <rpdirwk1025@naver.com>
2024-05-27 19:53:48 +0900

    스프링 추상화 예외 처리 적용

--- SpringDB/DB1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
+++ SpringDB/DB1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -1,30 +1,25 @@
 package hello.jdbc.service;
-
-import hello.jdbc.Repository.MemberRepositoryV3;
-import hello.jdbc.Repository.MemberRepositoryV4_1;
+import hello.jdbc.Repository.MemberRepository;
 import hello.jdbc.domain.Member;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.SQLException;
-
 /**
  * 예외 누수 문제 해결
  * SQLException 제거
+ *
  * MemberRepository 인터페이스 의존
  */
 @Slf4j
+@RequiredArgsConstructor
 public class MemberServiceV4 {
-    private final MemberRepositoryV4_1 memberRepository;
-
-    public MemberServiceV4(MemberRepositoryV4_1 memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    private final MemberRepository memberRepository;
 
     @Transactional
-    public void accountTransfer(String fromId, String toId, int money){
-         bizLogic(fromId, toId, money);
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
     }
+
     private void bizLogic(String fromId, String toId, int money) {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
@@ -32,6 +27,7 @@ public class MemberServiceV4 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
+
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
