68fe98068e4687583d56ba91869ceed39713e48c
KaarfSub <rpdirwk1025@naver.com>
2024-07-04 15:35:39 +0900

    필드 동기화 - 적용

---
+++ SpringAdvanced/advanced/src/main/java/hello/advanced/logtrace/LogTraceConfig.java
@@ -0,0 +1,12 @@
+package hello.advanced.logtrace;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
