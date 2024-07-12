8183b5fca143038bd0e93a8834ae5b22ffe39988
KaarfSub <rpdirwk1025@naver.com>
2024-07-12 15:16:06 +0900

    데코레이터 패턴 예제 - 체인 추가, TimeDecorator

--- SpringAdvanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ SpringAdvanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +19,15 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageComponent = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
