dc066e6f3104fb5b40b67a5b8542d0954c638d4b
KaarfSub <rpdirwk1025@naver.com>
2024-07-22 15:35:40 +0900

    프록시 팩토리 적용

--- SpringAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.DynamicProxyBasicConfig;
-import hello.proxy.config.DynamicProxyFilterConfig;
+import hello.proxy.config.*;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
@@ -18,7 +15,8 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfig1.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
