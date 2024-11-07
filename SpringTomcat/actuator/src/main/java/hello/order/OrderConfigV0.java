89e7b35908ee369731c1f826952ab3ca8ea81948
KaarfSub <rpdirwk1025@naver.com>
2024-11-07 15:11:53 +0900

    메트릭 등록 - 카운터

--- SpringTomcat/actuator/src/main/java/hello/order/OrderConfigV0.java
+++ SpringTomcat/actuator/src/main/java/hello/order/OrderConfigV0.java
@@ -3,7 +3,7 @@ package hello.order;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class OrderConfigV0 {
     @Bean
     OrderService orderService() {
