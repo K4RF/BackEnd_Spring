1f09361831cfdf51768e78d5372293f48c25275a
KaarfSub <rpdirwk1025@naver.com>
2024-11-07 15:53:19 +0900

    메트릭 등록 - 게이지

--- SpringTomcat/actuator/src/main/java/hello/ActuatorApplication.java
+++ SpringTomcat/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,6 +1,8 @@
 package hello;
 
 import hello.order.*;
+import hello.order.gauge.StockConfigV1;
+import hello.order.gauge.StockConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +13,9 @@ import org.springframework.context.annotation.Import;
 //@Import(OrderConfigV1.class)
 //@Import(OrderConfigV2.class)
 //@Import(OrderConfigV3.class)
-@Import(OrderServiceV4.class)
+//@Import({OrderServiceV4.class, StockConfigV1.class})
+@Import({OrderServiceV4.class, StockConfigV2.class})
+
 @SpringBootApplication
 public class ActuatorApplication {
 
